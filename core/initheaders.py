57cffc779f86c82c23b44610b418ea1331d12a44
AleksandrTan <rumych2013@gmail.com>
2021-03-04 15:50:44 +0200

    web bot start 1.0

--- core/initheaders.py
+++ core/initheaders.py
@@ -14,21 +14,19 @@ class InitHeaders:
     def make_headers(self):
         self.initialization_headers["User-Agent"] = instadata.USER_AGENT
         self.initialization_headers["Content-Length"] = 288
-        self.initialization_headers["X-IG-WWW-Claim"] = 0
-        self.initialization_headers["X-Instagram-AJAX"] = '63a87693d17'
         self.initialization_headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
         self.initialization_headers["Host"] = self.default_headers['Host']
-        self.initialization_headers["X-CSRFToken"] = ''
-        self.initialization_headers["X-IG-App-ID"] = 936619743392459
         self.initialization_headers["Sec-Fetch-Site"] = 'same-origin'
         self.initialization_headers["Sec-Fetch-Mode"] = 'cors'
         self.initialization_headers["Sec-Fetch-Dest"] = 'empty'
         self.initialization_headers["Referer"] = 'https://www.instagram.com/'
         self.initialization_headers["Accept-Encoding"] = 'gzip, deflate, br'
         self.initialization_headers["Accept-Language"] = self.default_headers["Accept-Language"]
-        self.initialization_headers["Cookie"] = 'ig_did=1106BD6C-3DD6-4EBD-B353-05948C7AA16D; ' \
-                                                'csrftoken=GmzQl4Y91OWXCP3Boce3AZRMHDAw8dKF; ' \
-                                                'mid=YD-SiwAEAAELCH-p293ePwQOkApD; ig_nrcb=1'
+        self.initialization_headers["x-requested-with"] = "XMLHttpRequest"
+        self.initialization_headers["Cookie"] = ''
+        self.initialization_headers["x-csrftoken"] = ""  # csrftoken cookie
+        self.initialization_headers["x-ig-app-id"] = ""
+        self.initialization_headers["x-ig-www-claim"] = ""
 
     def set_attribute_headers(self, key, value):
         self.initialization_headers[key] = value
--- socialapimodule/loginrequest.py
+++ socialapimodule/loginrequest.py
@@ -5,46 +5,41 @@ from logsource.logconfig import logger
 
 
 def login(account_data, initialization_parameters, initialization_headers, initialization_cookies, requests_map):
-
     headers = dict()
-    cookies = {
-        "csrftoken": 'csrftoken', "mid": 'mid', "ig_did": 'ig_did', "ds_user_id": 'ds_user_id',
-        "sessionid": 'sessionid', "ig_nrcb": 'ig_nrcb', "rur": 'rur'}
+    cookies = {}
 
-    # def interceptor(request):
-    #     try:
-    #         new = request.headers["Cookie"].split(';')
-    #         for new_one in new:
-    #             cooka = new_one.split('=')
-    #             cookies[cooka[0].strip()] = cooka[1].strip()
-    #     except AttributeError as error:
-    #         pass
-    #
-    # # Create a new instance of the Chrome driver
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--window-size=10x10")
-    # driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
-    #
-    # driver.get(requests_map["main_url"])
-    # time.sleep(5)
-    # username_field = driver.find_element_by_name('username')
-    # password_field = driver.find_element_by_name('password')
-    # submit_elem = driver.find_element_by_tag_name('button')
-    # username_field.send_keys(account_data["username"])
-    # password_field.send_keys(account_data["password"])
-    # submit_elem.click()
-    # driver.request_interceptor = interceptor
-    # request = driver.wait_for_request(requests_map["login"]["uri"])
-    # # print('-' * 50)
-    # # print(request.body, request.headers, request.querystring, 80001)
-    # # print('-' * 50)
-    # # print('* response' * 50)
-    # # print(request.response, request.response.headers, 9000)
-    # # print('* response' * 50)
-    # driver.close()
+    def interceptor(request):
+        try:
+            headers["x-ig-app-id"] = request.headers["X-IG-App-ID"]
+            new = request.headers["Cookie"].split(';')
+            for new_one in new:
+                cooka = new_one.split('=')
+                cookies[cooka[0].strip()] = cooka[1].strip()
+        except AttributeError as error:
+            pass
+
+    # Create a new instance of the Chrome driver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--window-size=10x10")
+    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
+
+    driver.get(requests_map["main_url"])
+    time.sleep(5)
+    username_field = driver.find_element_by_name('username')
+    password_field = driver.find_element_by_name('password')
+    submit_elem = driver.find_element_by_tag_name('button')
+    username_field.send_keys(account_data["username"])
+    password_field.send_keys(account_data["password"])
+    submit_elem.click()
+
+    driver.request_interceptor = interceptor
+    request = driver.wait_for_request(requests_map["login"]["uri"])
+    headers["x-ig-www-claim"] = request.response.headers["x-ig-set-www-claim"]
+    driver.close()
+    headers["x-csrftoken"] = cookies["csrftoken"]
 
     set_cookies(initialization_cookies, cookies)
-    set_headers(initialization_headers)
+    set_headers(initialization_headers, headers, cookies)
 
     return {"status": True}
 
@@ -54,5 +49,8 @@ def set_cookies(initialization_cookies: object, cookies: dict):
         setattr(initialization_cookies, cooks, cookies[cooks])
 
 
-def set_headers(initialization_headers: object):
-    initialization_headers.set_attribute_headers("X-CSRFToken", "1234567yurjmgDFGDGHB")
+def set_headers(initialization_headers: object, headers: dict, cookies: dict):
+    initialization_headers.set_attribute_headers("x-csrftoken", headers["x-csrftoken"])
+    initialization_headers.set_attribute_headers("x-ig-www-claim", headers["x-ig-www-claim"])
+    initialization_headers.set_attribute_headers("x-ig-app-id", headers["x-ig-app-id"])
+    initialization_headers.set_attribute_headers("Cookie", cookies)
